cc30d7c13db5779fc5749f248dfeed38bbe25b8a
Isoftware23 <139691450+Isoftware23@users.noreply.github.com>
2023-08-09 18:41:15 +0200

    b

---
+++ login.js
@@ -0,0 +1,20 @@
+let login={
+    name="c",
+   prompt:()=>{let username=prompt('username');
+let a=username},
+}
+let Willy_Renzetti={
+    name="Willy",
+    Surname="Renzetti",
+    id='wr',
+    Pass:'Choubidoux2318'
+
+}
+let Corrine={
+    id='co',
+    Pass='Choubidoux2318'
+
+}
+if(login.prompt.username=='wr'){
+    window.location.href(w.html)
+}
